3aab77b5caf57d9fed76060bf74520db145933e0
danielmkim22 <64539945+danielmkim22@users.noreply.github.com>
2020-04-30 01:57:11 +0900

    Delete Example.java

--- Example.java
+++
@@ -1,6 +0,0 @@
-echo "# Repository_Name" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/user_name/Repository_Name.git
-git push -u origin master
